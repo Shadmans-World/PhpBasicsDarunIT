db54083058738898c5f186f916eac88afbf0e31b
সাদমান সৌমিক শাওন <81482873+Shadmans-World@users.noreply.github.com>
2023-01-11 21:57:58 +0600

    Second Commit

---
+++ secondclass.php
@@ -0,0 +1,67 @@
+<?php
+// $a = 100;
+// $b = 20;
+// $c = 599;
+
+// if($a > $b && $a > $c){
+//     echo "A is the biggest";
+// }
+// else if ($a < $b && $b > $c){
+//     echo "B is the Biggest";
+// }
+// else
+//     echo "C is the Biggest";
+//*************************************************************************************************** */
+// $marks = 03;
+
+// if($marks >= 80 && $marks<=100){
+//     echo "Your Grade is A+ <br>";
+//     echo "Outsanding";
+// }
+// else if($marks >= 75 && $marks<=79){
+//     echo "Your Grade is A <br>";
+//     echo "Excellent";
+// }
+// else if ($marks >= 70 && $marks<=74){
+//     echo "Your Grade is A- <br>";
+//     echo "Very Good";
+// }
+// else if ($marks >= 65 && $marks<=69){
+//     echo "Your Grade is B+ <br>";
+//     echo "Good";
+// }
+// else if($marks >= 60 && $marks<=64){
+//     echo "Your Grade is B <br>";
+//     echo "Satisfactory";
+// }
+// else if ($marks >= 55 && $marks<=59){
+//     echo "Your Grade is B- <br>";
+//     echo "Above Average";
+// }
+// else if ($marks >= 50 && $marks<=54){
+//     echo "Your Grade is C+ <br>";
+//     echo "Average";
+// }
+// else if ($marks >= 45 && $marks<=49){
+//     echo "Your Grade is C <br>";
+//     echo "Below Average";
+// }
+// else if ($marks >= 40 && $marks<=44){
+//     echo "Your Grade is D <br>";
+//     echo "Pass";
+// }
+// else if ($marks >= 00 && $marks<=39){
+//     echo "Your Grade is F <br>";
+//     echo "Fail";
+// }
+// else{
+//     echo "Invalid Number";
+// }
+//**************************************************************************************** */
+for($i=0; $i<=100; $i++){
+    if($i%2 != 0){
+        echo $i.'<br>';
+    }
+}
+
+?>
