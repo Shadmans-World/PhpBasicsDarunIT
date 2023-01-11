4db906088facc3d23d5eb808503c321f38b3636d
সাদমান সৌমিক শাওন <81482873+Shadmans-World@users.noreply.github.com>
2023-01-11 23:42:23 +0600

    Fourth Commit

---
+++ fourthclass.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form In PHP</title>
+</head>
+<body>
+    <form action="contact.php" method="POST">
+        <label>Username</label>
+        <input type="text" name="user_name"><br>
+        <br>
+        <label>Email</label>
+        <input type="email" name="email"><br>
+        <br>
+        <label for="">Password</label>
+        
+        <input type="password" name="password"><br>
+        <br>
+        <input type="submit" value="submit data" name="btn">
+
+    </form>
+</body>
+</html>
+
