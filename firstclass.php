60f1532673cff98485ad811482ce8b204eff199f
সাদমান সৌমিক শাওন <81482873+Shadmans-World@users.noreply.github.com>
2023-01-11 20:57:28 +0600

    First Commit

---
+++ firstclass.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Hello World";
+echo "<br>";
+$message = "Hey Planet";
+echo $message;
+
+// $one = "Hello 12333"; //string
+// $two = 123; //integer
+// $three = 12.5; //float
+// $four = true; //boolean
+// $five = array(1223, 'Shadman', 12.5);  //array
+// $six = NULL; //NULL
+// echo "<br>";
+// echo var_dump($three);
+
+$shadman_age = 23;
+$arshi_age = 24; 
+echo "<br>";
+if($shadman_age > $arshi_age){
+    echo "Shadman is older than Arshi";
+}
+else
+   echo "Arshi is younger than Shadman";
+?>
